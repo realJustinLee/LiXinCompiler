11c4ee052a6b31f3edcae788282e8a1c1c3df2dd
李欣 <JustinDellAdam@live.com>
2018-04-12 14:01:04 +0800

    Parser init and compile

--- src/com/lixin/parser/Parser.java
+++ src/com/lixin/parser/Parser.java
@@ -1,16 +1,41 @@
 package com.lixin.parser;
 
 import com.lixin.lexer.Lexer;
+import com.lixin.lexer.Token;
+import com.lixin.symbols.Environment;
+
+import java.io.IOException;
 
 /**
  * @author lixin
  */
 public class Parser {
-    public Parser(Lexer lexer) {
+    private Lexer lexer;
+    private Token look;
+    Environment top = null;
+    int used = 0;
 
+    public Parser(Lexer lexer) throws IOException {
+        this.lexer = lexer;
+        move();
+    }
+
+    void move() throws IOException {
+        look = lexer.scan();
+    }
+
+    void error(String message) {
+        throw new Error("near line " + Lexer.line + ": " + message);
+    }
+
+    void match(int tag) throws IOException {
+        if (look.tag == tag) {
+            move();
+        } else error("syntax error");
     }
 
     public void program() {
 
     }
+
 }
