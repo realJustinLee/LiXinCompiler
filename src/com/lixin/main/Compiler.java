5f2a6435d7169b388f22539a6af7275c015aad20
李欣 <JustinDellAdam@live.com>
2018-05-30 21:55:49 +0800

    finally ok

--- src/com/lixin/main/Compiler.java
+++ src/com/lixin/main/Compiler.java
@@ -2,17 +2,17 @@ package com.lixin.main;
 
 import com.lixin.lexer.Lexer;
 import com.lixin.parser.Parser;
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
+import java.io.*;
 
 /**
  * @author lixin
  */
 public class Compiler {
     public void compile(File in, File out) {
+        InputStream sysIn = System.in;
+        PrintStream sysOut = System.out;
         try {
             FileInputStream fileInputStream = new FileInputStream(in);
             System.setIn(fileInputStream);
@@ -26,6 +26,33 @@ public class Compiler {
             System.out.println();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            System.setIn(sysIn);
+            System.setOut(sysOut);
+        }
+    }
+
+    public String compile(String in) {
+        InputStream sysIn = System.in;
+        PrintStream sysOut = System.out;
+        try {
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(in.getBytes());
+            System.setIn(inputStream);
+            ByteOutputStream outputStream = new ByteOutputStream();
+            PrintStream printStream = new PrintStream(outputStream);
+            System.setOut(printStream);
+            System.out.println("lxc>");
+            Lexer lexer = new Lexer();
+            Parser parser = new Parser(lexer);
+            parser.program();
+            System.out.println();
+            return outputStream.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            System.setIn(sysIn);
+            System.setOut(sysOut);
         }
     }
 }
--- src/com/lixin/main/CompilerTestDriver.java
+++ src/com/lixin/main/CompilerTestDriver.java
@@ -1,15 +1,33 @@
 package com.lixin.main;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
 /**
  * @author lixin
  */
 public class CompilerTestDriver {
+    static String in;
+    static String out;
+
     public static void main(String[] args) {
         File fileIn = new File("/Users/lixin/Downloads/in.txt");
         File fileOut = new File("/Users/lixin/Downloads/out.txt");
         Compiler compiler = new Compiler();
         compiler.compile(fileIn, fileOut);
+
+        in = "{\n" +
+                "\tint i; int j; float v; float x; float[100] a;\n" +
+                "\twhile( true ) {\n" +
+                "\t\tdo i = i+1; while( a[i] < v);\n" +
+                "\t\tdo j = j-1; while( a[j] > v);\n" +
+                "\t\tif( i >= j ) break;\n" +
+                "\t\tx = a[i]; a[i] = a[j]; a[j] = x;\n" +
+                "\t}\n" +
+                "}\n" +
+                "{}";
+        out = compiler.compile(in);
+        System.out.print(out);
     }
 }
