8630be880cb546668c2d1f75dd9a00a112f26d9b
熊某人 <840646587@qq.com>
2018-05-31 12:14:57 +0800

    添加hibernate中文支持

--- Online Compiler/src/javaee/test/Test.java
+++ Online Compiler/src/javaee/test/Test.java
@@ -5,12 +5,6 @@ import javaee.method.*;
 public class Test {
     public static void main(String[] args) {
         Code code = new Code();
-        System.out.println(code.GetByUsername("testaccount1")[0].getContent());
-        System.out.println(code.GetById(2).getTitle());
-
-        Bulletin bulletin = new Bulletin();
-        System.out.println(bulletin.GetById(4).getContent());
-        System.out.println(bulletin.GetByCodeId(1)[0].getContent());
-        System.out.println(bulletin.GetByUsername("testaccount1")[0].getContent());
+        code.Save("testaccount1","啊啊啊","啊啊");
     }
 }
