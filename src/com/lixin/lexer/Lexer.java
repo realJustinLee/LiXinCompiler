780b1ffec907664fd8c5306bebdc15340342cb6b
李欣 <JustinDellAdam@live.com>
2018-05-31 08:47:54 +0800

    '\r' problem fix

--- src/com/lixin/lexer/Lexer.java
+++ src/com/lixin/lexer/Lexer.java
@@ -63,6 +63,7 @@ public class Lexer {
             switch (peek) {
                 case ' ':
                 case '\t':
+                case '\r':
                     readChar();
                     continue;
                 case '\n':
